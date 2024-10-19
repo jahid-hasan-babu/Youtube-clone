56ff6c3fd695078fd6822a952024c3fa92e63af8
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-10-19 10:53:11 +0600

    update tweet

--- Backend/src/controllers/tweet.controller.js
+++ Backend/src/controllers/tweet.controller.js
@@ -27,26 +27,89 @@ const createTweet = asyncHandler(async (req, res) => {
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
-  const userId = req.params.userId;
+  const { userId } = req.params;
 
   if (!isValidObjectId(userId)) {
-    throw new ApiError(400, "Invalid User ID format");
+    throw new ApiError(400, "Invalid User ID");
   }
 
-  const user = await User.findById(userId);
-  if (!user) {
-    throw new ApiError(404, "User not found");
+  const userTweets = await Tweet.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweet",
+        as: "likeDetails",
+        pipeline: [
+          {
+            $project: {
+              likedBy: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+        totalLikes: {
+          $size: "$likeDetails",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likeDetails.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        content: 1,
+        owner: 1,
+        totalLikes: 1,
+        isLiked: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+
+  if (!userTweets.length) {
+    throw new ApiError(400, "No tweets found for this user");
   }
 
-  const tweets = await Tweet.find({ owner: userId });
-
-  if (!tweets.length) {
-    throw new ApiError(404, "No tweets found for this user");
-  }
-
-  res
+  return res
     .status(200)
-    .json(new ApiResponse(200, tweets, "User tweets retrieved successfully"));
+    .json(new ApiResponse(200, userTweets, "User tweets fetched successfully"));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
@@ -60,6 +123,12 @@ const updateTweet = asyncHandler(async (req, res) => {
   if (!content) {
     throw new ApiError(400, "Content is required for update");
   }
+
+  const tweet = await Tweet.findById(tweetId);
+
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
   const updatedTweet = await Tweet.findByIdAndUpdate(
     tweetId,
     { content },
@@ -68,7 +137,10 @@ const updateTweet = asyncHandler(async (req, res) => {
     }
   );
   if (!updatedTweet) {
-    throw new ApiError(404, "Tweet not found");
+    throw new ApiError(
+      400,
+      "Failed to update the tweet as tweetId couldn't be fetched"
+    );
   }
   res
     .status(200)
@@ -82,11 +154,18 @@ const deleteTweet = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Invalid Tweet ID format");
   }
 
-  const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
-  if (!deletedTweet) {
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
     throw new ApiError(404, "Tweet not found");
   }
-  res.status(200).json(new ApiResponse(200, "Tweet deleted successfully"));
+
+  const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
+  if (!deletedTweet) {
+    throw new ApiError(400, "Tweet not found and thus failed to delete");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Tweet deleted successfully"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
